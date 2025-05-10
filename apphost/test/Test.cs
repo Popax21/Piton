57bbfa270b9b1c22af3b260e4f4a2c1a870ff201
wartori <wartori54@gmail.com>
2025-05-10 17:28:47 +0200

    Update test c# project to net8

---
+++ apphost/test/Test.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+#pragma warning disable CS8600
+#pragma warning disable CS8602
+try {
+	((object)null).GetType();
+} catch (NullReferenceException) {
+	Console.WriteLine("NRE caught!");
+}
