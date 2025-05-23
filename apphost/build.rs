825a318d3218bd366ee08c0de45c4e283de56afa
wartori <wartori54@gmail.com>
2025-05-23 23:46:52 +0200

    Build c# test app in build.rs

--- apphost/build.rs
+++ apphost/build.rs
@@ -1,4 +1,5 @@
 use embed_manifest::{embed_manifest, new_manifest};
+use std::process::{Command};
 
 fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
@@ -9,5 +10,15 @@ fn main() {
         embed_manifest(new_manifest("Piton")).expect("unable to embed manifest file");
     }
 
+
+    if std::env::var_os("CARGO_FEATURE_TESTAPP").is_some() {
+        println!("cargo:rerun-if-changed=test");
+        Command::new("dotnet")
+            .arg("build")
+            .arg("test/Test.csproj")
+            .status()
+            .expect("Failed to build test C#");
+    }
+    
     println!("cargo:rerun-if-changed=build.rs")
 }
