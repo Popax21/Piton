52679d3a17ae7e94b38302320689e86659c0e775
Wartori <72220838+Wartori54@users.noreply.github.com>
2025-05-26 02:40:56 +0200

    Remove signal handler alt-stack to provide a stable runtime (#5)
    * Remove rust's altstack and sigsegv handler
    
    * Update test c# project to net8
    
    * Fix Cargo.lock version to 3
    
    * Force toolchain version for linux and osx
    
    * Explain the test case
    
    * Move fix to a module and revise comments, in line with the PR feedback
    
    * Build c# test app in build.rs
    
    * Apply changes from cargo fmt
    
    * Ignore dotnet build artifacts

---
+++ apphost/test/Test.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+#pragma warning disable CS8600 // Intentional NRE for testing purposes
+#pragma warning disable CS8602 // See above
+// Test if NREs work properly since by default rust uses a signal handler altstack which can 
+// be too small for the runtime to work with, see https://github.com/dotnet/runtime/issues/115438
+// for the full details
+
+// Piton currently includes a hacky fix to work around that
+
+try {
+	((object)null).GetType();
+} catch (NullReferenceException) {
+	Console.WriteLine("NRE caught!");
+}
